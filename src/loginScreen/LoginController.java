6dfddb440ef00e44e3d05beb31020597fa0dba58
mataratata21 <fil.mateja@gmail.com>
2021-05-24 16:59:05 +0200

    first screen done, added some assets

---
+++ src/loginScreen/LoginController.java
@@ -0,0 +1,10 @@
+package loginScreen;
+
+import javafx.application.Platform;
+
+public class LoginController {
+
+    public void exitApp(){
+        Platform.exit();
+    }
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,18 +1,24 @@
-package sample;
+package main;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        //smoothing za fontove
+        System.setProperty("prism.lcdtext", "false");
+        System.setProperty("prism.text", "t2k");
+        //izbaci windows bordere
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        Parent root = FXMLLoader.load(getClass().getResource("/loginScreen/login.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 1000, 600));
         primaryStage.show();
     }
 
