84616167f7c74b1412cff44a715452fc46d6d15d
tesicivan <82541067+tesicivan@users.noreply.github.com>
2021-06-12 11:06:38 +0200

    Update SQLMetode.java

--- src/util/SQLMetode.java
+++ src/util/SQLMetode.java
@@ -1,11 +1,19 @@
 package util;
 
+import DAOUtil.DatabaseInterface;
+import models.Device;
+import models.User;
+import models.WorkAction;
+import models.WorkApplication;
+import simulacija.Sonda;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.List;
 
-public class SQLMetode {
+public class SQLMetode implements DatabaseInterface {
 
     private Connection konekcija = null;
 
@@ -45,5 +53,35 @@ public class SQLMetode {
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
+    }
+
+    @Override
+    public User getUserFromDB(String username, String password, int type) {
+        return null;
+    }
+
+    @Override
+    public List<Device> getAllDevices() {
+        return null;
+    }
+
+    @Override
+    public List<List<Integer>> getParams(Sonda sonda) {
+        return null;
+    }
+
+    @Override
+    public List<WorkAction> getAllWorkActions() {
+        return null;
+    }
+
+    @Override
+    public List<WorkApplication> getWorkApplications(int actionId) {
+        return null;
+    }
+
+    @Override
+    public void createWorkAction(String name, String location, String time, int idAdmin) {
+
     }
 }
